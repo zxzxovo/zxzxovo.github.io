f11f64e15b26f23d9aea887c06a0f30cdd8e9c20
zxzxovo <zhixiaovo@gmail.com>
2025-04-08 14:21:52 +0000

    Deploy: set again

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/zxzxovo.github.io/sw.js', { scope: '/zxzxovo.github.io/' })})}
