4a7af62ebd4d7d682994408e6f2f0b31946ec46a
zxzxovo <zhixiaovo@gmail.com>
2025-04-08 16:31:13 +0000

    Deploy: setup again and again

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('https://hizhixia.site/sw.js', { scope: 'https://hizhixia.site/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
